a5ddfbfda1154956108869410cb681f7637733c3
deventorum <deventorum@gmail.com>
2020-07-16 12:20:51 -0400

    Gitignore + basic npm packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Normalize } from 'styled-normalize';
+// import GlobalStyles from './styles/GlobalStyles';
+
+// import Home from './components/Main/Home';
+// import TopNav from './components/TopNav/TopNav';
 
 function App() {
   return (
     <div className="App">
-      <h1> KuLGaP </h1>
+      <h1>KulGap</h1>
+      <Normalize />
+      {/* <GlobalStyles /> */}
+      {/* <Home /> */}
     </div>
   );
 }
