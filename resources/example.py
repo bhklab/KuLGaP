c3312076c282f733c3a48d251bbeed1027ec033f
deventorum <deventorum@gmail.com>
2020-07-23 10:46:15 -0400

    Code cleanup

--- resources/example.py
+++ resources/example.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 
 class ExampleData(Resource):
   def get(self):
+    print('Getting example data')
     # using txt file because json file cannot store NaN values
     with open('./example/patient.txt', 'r') as patient_file:
         patient = json.load(patient_file)
@@ -11,6 +12,6 @@ class ExampleData(Resource):
         summary = json.load(summary_file)
     # converts existing dictionaries into a single json and removes NaN values from it
     return simplejson.dumps({
-        "patient": patient,
-        "summary": summary
+        'patient': patient,
+        'summary': summary
     }, ignore_nan=True), 200
