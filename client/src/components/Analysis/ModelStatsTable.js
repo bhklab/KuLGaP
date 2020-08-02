c8401398f9eed559721f9ba97942d910d0e703ad
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-02 12:47:03 -0400

    ongoing model stats changes.

--- client/src/components/Analysis/ModelStatsTable.js
+++ client/src/components/Analysis/ModelStatsTable.js
@@ -45,27 +45,51 @@ const columns = [
     },
 ];
 
-const parseData = (data) => {
-    // this will create newData array of objects for the table.
-    const newData = [];
-    let total = 0;
-    let drugValue = '';
-    let modelValue = '';
+// const parseData = (data) => {
+//     // this will create newData array of objects for the table.
+//     const newData = [];
+//     let total = 0;
+//     let drugValue = '';
+//     let modelValue = '';
 
-    data.forEach((eachdata) => {
-        if (newData.length === 0 || drugValue !== eachdata.drug_name || modelValue !== eachdata.model) {
-            newData.push({});
-            drugValue = eachdata.drug_name;
-            modelValue = eachdata.model;
-            newData[total].model = modelValue;
-            newData[total].drug = drugValue;
-            newData[total].patient = eachdata.patient;
-            newData[total].link = eachdata.link;
-            newData[total].row = eachdata.row;
-            total += 1;
-        }
-        newData[total - 1][eachdata.response_type === 'best.average.response' ? 'bar' : eachdata.response_type] = eachdata.value;
+//     data.forEach((eachdata) => {
+//         if (newData.length === 0 || drugValue !== eachdata.drug_name || modelValue !== eachdata.model) {
+//             newData.push({});
+//             drugValue = eachdata.drug_name;
+//             modelValue = eachdata.model;
+//             newData[total].model = modelValue;
+//             newData[total].drug = drugValue;
+//             newData[total].patient = eachdata.patient;
+//             newData[total].link = eachdata.link;
+//             newData[total].row = eachdata.row;
+//             total += 1;
+//         }
+//         newData[total - 1][eachdata.response_type === 'best.average.response' ? 'bar' : eachdata.response_type] = eachdata.value;
+//     });
+//     return newData;
+// };
+
+const parseData = (data) => {
+    console.log(data);
+    const newData = [];
+    const cleanData = (value) => JSON.parse(JSON.stringify(value)).split('_').map((val) => val.replace(/[0-9].*:/, ''));
+    cleanData(data.auc).forEach((val) => {
+        newData.push({
+            AUC: val,
+            drug: data.drug,
+            patient: 'unknown',
+            model: 'unknown',
+        });
     });
+    cleanData(data.auc_control).forEach((val) => {
+        newData.push({
+            AUC: val,
+            drug: 'control',
+            patient: 'unknown',
+            model: 'unknown',
+        });
+    });
+
     return newData;
 };
 
