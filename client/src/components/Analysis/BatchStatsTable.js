8f88de1648c2a5accc9cdc852f305ea7d4e76b3f
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-31 21:10:38 -0400

    code for calculating the response.

--- client/src/components/Analysis/BatchStatsTable.js
+++ client/src/components/Analysis/BatchStatsTable.js
@@ -32,12 +32,39 @@ const values = [
     }, {
         key: 'average_angle',
         value: 'Angle',
+    }, {
+        key: 'mRECIST',
+        value: 'mRECIST',
     },
 ];
 
+const calcResponse = (data) => {
+    const count = {
+        SD: 0,
+        PR: 0,
+        PD: 0,
+        CR: 0,
+    };
+    let max = 0;
+    let max_array = [];
+    Object.entries(data.mRECIST).forEach((val) => {
+        count[val[1].replace('m', '')] += 1;
+    });
+    Object.entries(count).forEach((val) => {
+        if (val[1] > max) {
+            max_array = [];
+            max_array.push(val[0]);
+            max = val[1];
+        } else if (val[1] === max) {
+            max_array.push(val[0]);
+        }
+    });
+    return max_array.join('/');
+};
+
 const parseData = (data) => values.map((object) => ({
     name: object.value,
-    estimate: FixedPoint(data[object.key]),
+    estimate: object.value === 'mRECIST' ? calcResponse(data) : FixedPoint(data[object.key]),
     'p-value': object.key === 'kl' ? data.kl_p_value.toExponential(1) : '',
     responder: object.key === 'kl' ? (data.kl_p_value < 0.05 ? 'Yes' : 'No') : '',
 }));
