b9cd43f1375185afa1d9e20d436a7c2ac65023e3
Gangesh Beri <41119826+gangeshberi@users.noreply.github.com>
2021-12-05 17:04:46 -0500

    new build

---
+++ client/build/precache-manifest.7dee380c47b610b64381b9ea98a8092c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7bfbc2d05bcb30b02dba539624566a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea311069efe57e597658",
+    "url": "/static/css/2.d17711cb.chunk.css"
+  },
+  {
+    "revision": "ea311069efe57e597658",
+    "url": "/static/js/2.0ce35fbe.chunk.js"
+  },
+  {
+    "revision": "57c4115a3407cd34a0ac5252f2bed8f2",
+    "url": "/static/js/2.0ce35fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a361ff51574321303cc8",
+    "url": "/static/js/main.0501fa0a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a0f497be29a695c22e4da185b86dfa62",
+    "url": "/static/media/bg.a0f497be.png"
+  },
+  {
+    "revision": "f678bfa501f4bec32eb33380bb861ed4",
+    "url": "/static/media/download1.f678bfa5.svg"
+  },
+  {
+    "revision": "5e11750119497018b30918ec8e3bd8c7",
+    "url": "/static/media/graph.5e117501.png"
+  },
+  {
+    "revision": "7652025c179d9d543102ee762efbf8ae",
+    "url": "/static/media/input.7652025c.png"
+  },
+  {
+    "revision": "5b0771bcc2c89e636604f9a485664839",
+    "url": "/static/media/logo.5b0771bc.png"
+  },
+  {
+    "revision": "18dcf9c612589c0715f947b979919033",
+    "url": "/static/media/model.18dcf9c6.png"
+  },
+  {
+    "revision": "24e7ae09cd1aeae159bd4369fcd06f1a",
+    "url": "/static/media/stats.24e7ae09.png"
+  }
+]);
